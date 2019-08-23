0ed454bad4251e1fcc7be7077a586e7fe49bb2bd
Tejasvi Kothapalli <tejasvikothapalli@Tejasvis-MacBook-Pro.local>
2019-08-22 20:31:56 -0700

    killed and restarted server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-	host: 'ec2-54-193-77-222.us-west-1.compute.amazonaws.com',
+	host: 'ec2-52-53-196-168.us-west-1.compute.amazonaws.com',
       key: '~/desktop/pose.pem',
       ref: 'origin/master',
       repo: 'https://github.com/vitae-gravitas/pose-server.git',
