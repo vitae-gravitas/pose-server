f80e38d168b7842ba2d8d4b7b706072e9e47fa1c
Tejasvi Kothapalli <tejasvikothapalli@calvisitor-10-105-158-122.calvisitor.1918.berkeley.edu>
2019-08-29 15:13:40 -0700

    fixing ec2 link

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-	host: 'ec2-13-57-57-47.us-west-1.compute.amazonaws.com',
+	host: 'ec2-54-153-63-227.us-west-1.compute.amazonaws.com',
       key: '~/desktop/pose.pem',
       ref: 'origin/master',
       repo: 'https://github.com/vitae-gravitas/pose-server.git',
